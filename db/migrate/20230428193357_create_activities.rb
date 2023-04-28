3c4549049dd78f6ad03c4c26f5e310b80b7e522a
Siddharth Sawant <sid.sawant@kellogg.northwestern.edu>
2023-04-28 19:48:16 +0000

    ROR Commit4

--- db/migrate/20230428193357_create_activities.rb
+++ db/migrate/20230428193357_create_activities.rb
@@ -1,9 +1,9 @@
 class CreateActivities < ActiveRecord::Migration[7.0]
   def change
     create_table :activities do |t|
-      t.integer :salesperson_id
-      t.integer :contact_id
-      t.integer :note
+      t.integer :"salesperson_id"
+      t.integer :"contact_id"
+      t.integer :"note"
       t.timestamps
     end
   end
